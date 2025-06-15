f5223b6e399ec93e12f40925c939ff9d2f31d425
Eric Wang <eric88wang@qq.com>
2025-06-15 16:37:34 +0800

    init db

--- config/config.go
+++ config/config.go
@@ -8,7 +8,7 @@ func Init() {
 		panic(err)
 	}
 
-	// InitDB()
+	InitDB()
 }
 
 func LoadCfg() error {
---
+++ config/db.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	passwd := os.Getenv("POSTGRES_PASSWORD")
+	DB = openDB(
+		viper.GetString("db.host"),
+		viper.GetString("db.port"),
+		viper.GetString("db.username"),
+		passwd,
+		viper.GetString("db.name"),
+	)
+}
+
+func openDB(host, port, username, password, name string) *gorm.DB {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host,
+		username,
+		password,
+		name,
+		port)
+
+	newLogger := logger.New(
+		log.StandardLogger(),
+		logger.Config{
+			SlowThreshold: time.Second, // Slow SQL threshold
+			LogLevel:      logger.Info, // Log level
+			Colorful:      false,       // Disable color
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		log.Error(err)
+		log.Errorf("Database connection failed. Database name: %s", name)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Error(err)
+		log.Errorf("SetMaxIdleConns get an error.")
+	} else {
+		sqlDB.SetMaxOpenConns(20000)
+		sqlDB.SetMaxIdleConns(100)
+	}
+
+	log.Info("db connected")
+
+	return db
+}
